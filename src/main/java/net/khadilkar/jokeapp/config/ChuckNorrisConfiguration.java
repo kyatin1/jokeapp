c89dfd8f5dc77ddb6e1f0d8a816c3138b8507260
ykhadilkar <ykhadilkar@reisystems.com>
2018-07-19 09:02:28 -0400

    adding banner, moving out dependency to config class

---
+++ src/main/java/net/khadilkar/jokeapp/config/ChuckNorrisConfiguration.java
@@ -0,0 +1,14 @@
+package net.khadilkar.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckNorrisConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes ChuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
